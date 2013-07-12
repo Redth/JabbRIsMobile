43f6db5c59d3fd0619c00144447b27dfe534d3a9
Redth <jondick@gmail.com>
2013-07-12 10:38:53 -0400

    Updated user menu to use 'square' look

--- JabbrMobile.UI.Android/Converters/UserStatusDrawableValueConverter.cs
+++ JabbrMobile.UI.Android/Converters/UserStatusDrawableValueConverter.cs
@@ -1,6 +1,7 @@
 using System;
 using Cirrious.CrossCore.Converters;
 using Android.Graphics.Drawables;
+using Android.Graphics;
 using JabbR.Client.Models;
 using Android.Content.Res;
 using Android.Graphics;
@@ -18,6 +19,22 @@ namespace JabbrMobile.Android
 		}
 
 		protected override UserStatus ConvertBack (int value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return UserStatus.Active;
+		}
+	}
+
+	public class UserStatusColorValueConverter : MvxValueConverter<UserStatus, Color>
+	{
+		protected override Color Convert (UserStatus value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == UserStatus.Active)
+				return Color.Green;
+			else
+				return Color.Yellow;
+		}
+
+		protected override UserStatus ConvertBack (Color value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
 		{
 			return UserStatus.Active;
 		}
